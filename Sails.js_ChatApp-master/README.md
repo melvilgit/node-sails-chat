238ee3d36d1b400f709a58c5a4da1ed5dbbaf0b3
Melvil <melvil@LR74P93.(none)>
2013-11-25 14:39:50 +0530

    Bootstarp file added for css,package.json changed

--- Sails.js_ChatApp-master/README.md
+++
@@ -1,2 +0,0 @@
-# ChatApp
-### a Sails application
--- Sails.js_ChatApp-master/config/policies.js
+++
@@ -1,9 +0,0 @@
-/**
-* Policy defines middleware that is run before each controller/controller.
-* Any policy dropped into the /middleware directory is made globally available through sails.middleware
-* Below, use the string name of the middleware
-*/
-module.exports.policies = {	
-	'*': true,
-	'messages': 'MessagesPolicy'
-};
